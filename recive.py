303924d50d36354029a88745f17cea70732f0af0
Mohammad Abu-Shelbaia <mabushelbaia@gmail.com>
2023-07-13 14:41:12 +0300

    Center Frequency mismatch

--- recive.py
+++ recive.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-sdr = SDR("192.168.2.1")
+sdr = SDR("192.168.2.1", sample_rate=1e6, center_freq=50e6, num_samps=10000)
 
 ts = 1/float(sdr.sample_rate)
 t = np.arange(0, sdr.num_samps*ts, ts)
 sdr.rx_config(mode="manual", gain=50)
 rx_samples = sdr.recive()
 plt.plot(t, np.real(rx_samples))
+plt.plot(t, np.imag(rx_samples))
 plt.show()
--- transmit.py
+++ transmit.py
@@ -3,13 +3,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-sdr = SDR("192.168.2.1")
-sdr.tx_config("manual", 0.0)
+sdr = SDR("192.168.2.1", sample_rate=1e6, center_freq=100e6, num_samps=10000)
+sdr.tx_config("fast_attack", 0.0)
 wc =  2*np.pi*2000
 # Signal Configuration
 ts = 1/float(sdr.sample_rate)
 t = np.arange(0, sdr.num_samps*ts, ts)
 samples = sdr.signal(-np.sin(wc*t), np.cos(wc*t))
-while True
+plt.plot(t, np.real(samples))
+# plt.show()
+while True:
+    sdr.pluto.tx_destroy_buffer()
     sdr.transmit(samples)
     time.sleep(1)
