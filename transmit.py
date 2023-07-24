fab982b62fe8ffda72051750fc07f184ed1fbbce
Mohammad Abu-Shelbaia <mabushelbaia@gmail.com>
2023-07-24 16:51:25 +0300

    cyclic buffer

--- transmit.py
+++ transmit.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-sdr = SDR("192.168.2.1", sample_rate=1e6, center_freq=1000e6, num_samps=10000)
+sdr = SDR("192.168.2.1", sample_rate=1e6, center_freq=1000e6, num_samps=1000)
 sdr.tx_config("manual", 0.0)
 wc =  2*np.pi*2000
 # Signal Configuration
@@ -15,4 +15,5 @@ sdr.pluto.tx_destroy_buffer()
 sdr.pluto.tx_cyclic_buffer = True
 sdr.transmit(samples)
 plt.show()
+
     
